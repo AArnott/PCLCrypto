ca6c4f65de5b2ecab3b413ff409dd1bbbf6dafd5
Andrew Arnott <andrewarnott@gmail.com>
2014-02-23 17:19:19 -0800

    Adds Silverlight test target.

---
+++ src/PCLCrypto.Tests.Silverlight/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace PCLCrypto.Tests.Silverlight
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+    using System.IO.IsolatedStorage;
+    using PCLTesting.Infrastructure;
+
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void RunTestsButton_Click(object sender, RoutedEventArgs e)
+        {
+            RunTestsButton.IsEnabled = false;
+
+            try
+            {
+                var testRunner = new TestRunner(typeof(RandomNumberGeneratorTests).Assembly);
+                await testRunner.RunTestsAsync();
+                ResultsTextBox.Text = testRunner.Log;
+            }
+            catch (Exception ex)
+            {
+                ResultsTextBox.Text = ex.ToString();
+            }
+
+            RunTestsButton.IsEnabled = true;
+        }
+    }
+}
