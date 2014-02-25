4a22d5cff9131ed8b982b71debab24044b153ae5
Andrew Arnott <andrewarnott@gmail.com>
2014-02-25 08:49:03 -0800

    Adds tests for GetNonZeroBytes.

---
+++ src/PCLCrypto.Tests.Desktop/RandomNumberGeneratorDesktopTests.cs
@@ -0,0 +1,32 @@
+﻿namespace PCLCrypto.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PCLTesting;
+
+    [TestClass]
+    public class RandomNumberGeneratorDesktopTests
+    {
+        [TestMethod]
+        public void GetNonZeroBytes()
+        {
+            var rng = Crypto.RandomNumberGenerator as System.Security.Cryptography.RandomNumberGenerator;
+            Assert.IsNotNull(rng);
+            byte[] buffer = new byte[15];
+            rng.GetNonZeroBytes(buffer);
+            Assert.IsTrue(buffer.All(b => b != 0));
+        }
+
+        [TestMethod]
+        public void GetNonZeroBytes_Null()
+        {
+            var rng = Crypto.RandomNumberGenerator as System.Security.Cryptography.RandomNumberGenerator;
+            Assert.IsNotNull(rng);
+            ExceptionAssert.Throws<ArgumentNullException>(() => rng.GetNonZeroBytes(null));
+        }
+    }
+}
