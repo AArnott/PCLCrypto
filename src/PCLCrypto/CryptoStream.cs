bb90e131a5eab68a1e8b0a29aaf6237afcc2aaa1
Andrew Arnott <andrewarnott@gmail.com>
2014-03-01 13:00:24 -0800

    Fixes remaining StyleCop messages.

--- src/PCLCrypto/CryptoStream.cs
+++ src/PCLCrypto/CryptoStream.cs
@@ -36,32 +36,38 @@ namespace PCLCrypto
 
         #region Stream Properties
 
+        /// <inheritdoc />
         public override bool CanRead
         {
             get { throw new NotImplementedException(); }
         }
 
+        /// <inheritdoc />
         public override bool CanSeek
         {
             get { throw new NotImplementedException(); }
         }
 
+        /// <inheritdoc />
         public override bool CanWrite
         {
             get { throw new NotImplementedException(); }
         }
 
+        /// <inheritdoc />
         public override long Length
         {
             get { throw new NotImplementedException(); }
         }
 
+        /// <inheritdoc />
         public override long Position
         {
             get
             {
                 throw new NotImplementedException();
             }
+
             set
             {
                 throw new NotImplementedException();
@@ -83,26 +89,31 @@ namespace PCLCrypto
 
         #region Stream methods
 
+        /// <inheritdoc />
         public override void Flush()
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc />
         public override int Read(byte[] buffer, int offset, int count)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc />
         public override long Seek(long offset, SeekOrigin origin)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc />
         public override void SetLength(long value)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc />
         public override void Write(byte[] buffer, int offset, int count)
         {
             throw new NotImplementedException();
