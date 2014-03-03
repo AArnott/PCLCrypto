22bc7b1cd24dd59728e6e96aad203e1254952ad3
Andrew Arnott <andrewarnott@gmail.com>
2014-03-02 20:36:14 -0800

    WinRT now implements the streaming encryption/decryption API, although it's just a shim to the WinRT-limitation that everything must be in memory upfront.

--- src/PCLCrypto.WinRT/CryptographicEngine.cs
+++ src/PCLCrypto.WinRT/CryptographicEngine.cs
@@ -8,6 +8,7 @@ namespace PCLCrypto
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -34,7 +35,10 @@ namespace PCLCrypto
         /// <inheritdoc />
         public ICryptoTransform CreateEncryptor(ICryptographicKey key, byte[] iv)
         {
-            throw new NotImplementedException();
+            Requires.NotNull(key, "key");
+
+            return new BufferingCryptoTransform(
+                data => this.Encrypt(key, data, iv));
         }
 
         /// <inheritdoc />
@@ -52,7 +56,10 @@ namespace PCLCrypto
         /// <inheritdoc />
         public ICryptoTransform CreateDecryptor(ICryptographicKey key, byte[] iv)
         {
-            throw new NotImplementedException();
+            Requires.NotNull(key, "key");
+
+            return new BufferingCryptoTransform(
+                data => this.Decrypt(key, data, iv));
         }
 
         /// <inheritdoc />
@@ -123,6 +130,80 @@ namespace PCLCrypto
             Requires.NotNull(key, "key");
             var platformKey = ((CryptographicKey)key).Key;
             return platformKey;
+        }
+
+        /// <summary>
+        /// A crypto transform that can do no work incrementally, but does it all at the end.
+        /// </summary>
+        /// <remarks>
+        /// Sadly, this is necessary because WinRT offers no incremental encryption/decryption
+        /// APIs.
+        /// </remarks>
+        private class BufferingCryptoTransform : ICryptoTransform
+        {
+            /// <summary>
+            /// The buffering stream.
+            /// </summary>
+            private readonly MemoryStream bufferingStream = new MemoryStream();
+
+            /// <summary>
+            /// The transform to run when all bytes are collected.
+            /// </summary>
+            private readonly Func<byte[], byte[]> transform;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="BufferingCryptoTransform"/> class.
+            /// </summary>
+            /// <param name="transform">The transform to run when all bytes are collected.</param>
+            internal BufferingCryptoTransform(Func<byte[], byte[]> transform)
+            {
+                Requires.NotNull(transform, "transform");
+                this.transform = transform;
+            }
+
+            /// <inheritdoc />
+            public bool CanReuseTransform
+            {
+                get { return false; }
+            }
+
+            /// <inheritdoc />
+            public bool CanTransformMultipleBlocks
+            {
+                get { return true; }
+            }
+
+            /// <inheritdoc />
+            public int InputBlockSize
+            {
+                get { return 1; }
+            }
+
+            /// <inheritdoc />
+            public int OutputBlockSize
+            {
+                get { return 1; }
+            }
+
+            /// <inheritdoc />
+            public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+            {
+                this.bufferingStream.Write(inputBuffer, inputOffset, inputCount);
+                return 0;
+            }
+
+            /// <inheritdoc />
+            public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+            {
+                this.bufferingStream.Write(inputBuffer, inputOffset, inputCount);
+                return this.transform(this.bufferingStream.ToArray());
+            }
+
+            /// <inheritdoc />
+            public void Dispose()
+            {
+                this.bufferingStream.Dispose();
+            }
         }
     }
 }
