97b10e3539c05b18e32e2a9045190907d4d80ad1
Andrew Arnott <andrewarnott@gmail.com>
2014-03-02 21:54:28 -0800

    Improves Silverlight test runner UI.

--- src/PCLCrypto.Tests.Silverlight/MainPage.xaml.cs
+++ src/PCLCrypto.Tests.Silverlight/MainPage.xaml.cs
@@ -2,8 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
     using System.Net;
+    using System.Threading.Tasks;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Documents;
@@ -23,19 +25,31 @@
         private async void RunTestsButton_Click(object sender, RoutedEventArgs e)
         {
             this.RunTestsButton.IsEnabled = false;
+            this.TestRunProgress.Visibility = Visibility.Visible;
+            this.TextSummaryText.Visibility = Visibility.Collapsed;
 
             try
             {
                 var testRunner = new TestRunner(typeof(RandomNumberGeneratorTests).Assembly);
-                await testRunner.RunTestsAsync();
+                await Task.Factory.StartNew(() => testRunner.RunTestsAsync());
+                this.TextSummaryText.Text = string.Format(
+                    CultureInfo.CurrentCulture,
+                    "{0}/{1} tests passed ({2}%)",
+                    testRunner.PassCount,
+                    testRunner.TestCount,
+                    100 * testRunner.PassCount / testRunner.TestCount);
+                this.TextSummaryText.Visibility = Visibility.Visible;
                 this.ResultsTextBox.Text = testRunner.Log;
             }
             catch (Exception ex)
             {
                 this.ResultsTextBox.Text = ex.ToString();
             }
-
-            this.RunTestsButton.IsEnabled = true;
+            finally
+            {
+                this.RunTestsButton.IsEnabled = true;
+                this.TestRunProgress.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
