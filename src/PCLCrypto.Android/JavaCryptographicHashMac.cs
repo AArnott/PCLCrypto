5142f4dffc3aa10b5eceb4af86f89752d29db807
Andrew Arnott <andrewarnott@gmail.com>
2014-03-08 12:06:10 -0800

    Android now uses its native MAC algorithm instead of mono managed implementation.

---
+++ src/PCLCrypto.Android/JavaCryptographicHashMac.cs
@@ -0,0 +1,86 @@
+//-----------------------------------------------------------------------
+// <copyright file="JavaCryptographicHashMac.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PCLCrypto
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Android.App;
+    using Android.Content;
+    using Android.OS;
+    using Android.Runtime;
+    using Android.Views;
+    using Android.Widget;
+    using Java.Security;
+    using Javax.Crypto;
+    using Validation;
+
+    /// <summary>
+    /// A Java MessageDigest implementation of the <see cref="CryptographicHash"/> interface.
+    /// </summary>
+    internal class JavaCryptographicHashMac : CryptographicHash
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JavaCryptographicHashMac"/> class.
+        /// </summary>
+        /// <param name="algorithm">The algorithm.</param>
+        internal JavaCryptographicHashMac(Mac algorithm)
+        {
+            Requires.NotNull(algorithm, "algorithm");
+
+            this.Algorithm = algorithm;
+        }
+
+        /// <summary>
+        /// Gets the platform-specific hash algorithm.
+        /// </summary>
+        protected Mac Algorithm { get; private set; }
+
+        /// <inheritdoc />
+        public override void Append(byte[] data)
+        {
+            this.Algorithm.Update(data);
+        }
+
+        /// <inheritdoc />
+        public override byte[] GetValueAndReset()
+        {
+            byte[] result = this.Algorithm.DoFinal();
+            this.Algorithm.Reset();
+            return result;
+        }
+
+        /// <inheritdoc />
+        protected override int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+        {
+            this.Algorithm.Update(inputBuffer, inputOffset, inputCount);
+            if (outputBuffer != null)
+            {
+                Array.Copy(inputBuffer, inputOffset, outputBuffer, outputOffset, inputCount);
+            }
+
+            return inputCount;
+        }
+
+        /// <inheritdoc />
+        protected override byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+        {
+            this.Algorithm.Update(inputBuffer, inputOffset, inputCount);
+            if (inputCount == inputBuffer.Length)
+            {
+                return inputBuffer;
+            }
+            else
+            {
+                var buffer = new byte[inputCount];
+                Array.Copy(inputBuffer, inputOffset, buffer, 0, inputCount);
+                return buffer;
+            }
+        }
+    }
+}
---
+++ src/PCLCrypto.Android/MacAlgorithmProvider.cs
@@ -0,0 +1,96 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MacAlgorithmProvider.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PCLCrypto
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Javax.Crypto;
+    using Javax.Crypto.Spec;
+    using Validation;
+    using Platform = System.Security.Cryptography;
+
+    /// <summary>
+    /// The Android implementation of the <see cref="IMacAlgorithmProvider"/> interface.
+    /// </summary>
+    internal class MacAlgorithmProvider : IMacAlgorithmProvider
+    {
+        /// <summary>
+        /// The algorithm of this instance.
+        /// </summary>
+        private readonly MacAlgorithm algorithm;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MacAlgorithmProvider"/> class.
+        /// </summary>
+        /// <param name="algorithm">The algorithm.</param>
+        internal MacAlgorithmProvider(MacAlgorithm algorithm)
+        {
+            this.algorithm = algorithm;
+        }
+
+        /// <inheritdoc />
+        public MacAlgorithm Algorithm
+        {
+            get { return this.algorithm; }
+        }
+
+        /// <inheritdoc />
+        public int MacLength
+        {
+            get
+            {
+                using (var algorithm = GetAlgorithm(this.Algorithm))
+                {
+                    return algorithm.MacLength;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public CryptographicHash CreateHash(byte[] keyMaterial)
+        {
+            Requires.NotNull(keyMaterial, "keyMaterial");
+
+            var hash = GetAlgorithm(this.Algorithm);
+            hash.Init(GetSecretKey(this.Algorithm, keyMaterial));
+            return new JavaCryptographicHashMac(hash);
+        }
+
+        /// <inheritdoc />
+        public ICryptographicKey CreateKey(byte[] keyMaterial)
+        {
+            return new MacCryptographicKey(this.Algorithm, keyMaterial);
+        }
+
+        /// <summary>
+        /// Returns the keyed hash algorithm from the platform.
+        /// </summary>
+        /// <param name="algorithm">The algorithm desired.</param>
+        /// <returns>The platform-specific algorithm.</returns>
+        internal static Mac GetAlgorithm(MacAlgorithm algorithm)
+        {
+            string algorithmName = MacAlgorithmProviderFactory.GetAlgorithmName(algorithm);
+            return Mac.GetInstance(algorithmName);
+        }
+
+        /// <summary>
+        /// Returns the secret key to use for initializing the Mac.
+        /// </summary>
+        /// <param name="algorithm">The algorithm.</param>
+        /// <param name="keyMaterial">The key material.</param>
+        /// <returns>The secret key.</returns>
+        internal static SecretKeySpec GetSecretKey(MacAlgorithm algorithm, byte[] keyMaterial)
+        {
+            string algorithmName = MacAlgorithmProviderFactory.GetAlgorithmName(algorithm);
+            var signingKey = new SecretKeySpec(keyMaterial, algorithmName);
+            return signingKey;
+        }
+    }
+}
