fa293981c61f7b5b765c63bf89d502d1c5704724
Andrew Arnott <andrewarnott@gmail.com>
2014-03-08 08:17:21 -0800

    Adds known-good result to derive bytes test.

--- src/PCLCrypto.Tests/DeriveBytesTests.cs
+++ src/PCLCrypto.Tests/DeriveBytesTests.cs
@@ -14,6 +14,7 @@
         private const string Password1 = "Password";
         private static readonly byte[] Salt1 = new byte[] { 0x1, 0x2, 0x4, 0x5, 0x3, 0x6, 0x7, 0x8 };
         private static readonly byte[] Salt2 = new byte[] { 0x1, 0x3, 0x2, 0x5, 0x3, 0x6, 0x7, 0x8 };
+        private const string DerivedKey = "S23vp2E3VXpRGQ==";
 
         [TestMethod]
         public void GetBytes()
@@ -21,6 +22,7 @@
             byte[] keyFromPassword = NetFxCrypto.DeriveBytes.GetBytes(Password1, Salt1, 5, 10);
             byte[] keyFromBytes = NetFxCrypto.DeriveBytes.GetBytes(Encoding.UTF8.GetBytes(Password1), Salt1, 5, 10);
             CollectionAssertEx.AreEqual(keyFromPassword, keyFromBytes);
+            Assert.AreEqual(DerivedKey, Convert.ToBase64String(keyFromPassword));
 
             byte[] keyWithOtherSalt = NetFxCrypto.DeriveBytes.GetBytes(Password1, Salt2, 5, 10);
             CollectionAssertEx.AreNotEqual(keyFromPassword, keyWithOtherSalt);
