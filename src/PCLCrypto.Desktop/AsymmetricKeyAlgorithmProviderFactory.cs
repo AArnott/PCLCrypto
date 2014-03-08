e1a971f2dd7eda4ed1a1cef359f29ed44bc30240
Andrew Arnott <andrewarnott@gmail.com>
2014-03-08 11:06:38 -0800

    Reduced some switch statement duplication.

--- src/PCLCrypto.Desktop/AsymmetricKeyAlgorithmProviderFactory.cs
+++ src/PCLCrypto.Desktop/AsymmetricKeyAlgorithmProviderFactory.cs
@@ -58,5 +58,40 @@ namespace PCLCrypto
                     throw new NotSupportedException();
             }
         }
+
+        /// <summary>
+        /// Gets the hash algorithm for a given asymmetric algorithm.
+        /// </summary>
+        /// <param name="algorithm">The algorithm.</param>
+        /// <returns>A hash algorithm.</returns>
+        internal static HashAlgorithm GetHashAlgorithmEnum(AsymmetricAlgorithm algorithm)
+        {
+            switch (algorithm)
+            {
+                case AsymmetricAlgorithm.DsaSha1:
+                case AsymmetricAlgorithm.RsaOaepSha1:
+                case AsymmetricAlgorithm.RsaSignPkcs1Sha1:
+                case AsymmetricAlgorithm.RsaSignPssSha1:
+                    return HashAlgorithm.Sha1;
+                case AsymmetricAlgorithm.DsaSha256:
+                case AsymmetricAlgorithm.RsaOaepSha256:
+                case AsymmetricAlgorithm.EcdsaP256Sha256:
+                case AsymmetricAlgorithm.RsaSignPkcs1Sha256:
+                case AsymmetricAlgorithm.RsaSignPssSha256:
+                    return HashAlgorithm.Sha256;
+                case AsymmetricAlgorithm.EcdsaP384Sha384:
+                case AsymmetricAlgorithm.RsaOaepSha384:
+                case AsymmetricAlgorithm.RsaSignPkcs1Sha384:
+                case AsymmetricAlgorithm.RsaSignPssSha384:
+                    return HashAlgorithm.Sha384;
+                case AsymmetricAlgorithm.EcdsaP521Sha512:
+                case AsymmetricAlgorithm.RsaOaepSha512:
+                case AsymmetricAlgorithm.RsaSignPkcs1Sha512:
+                case AsymmetricAlgorithm.RsaSignPssSha512:
+                    return HashAlgorithm.Sha512;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
     }
 }
