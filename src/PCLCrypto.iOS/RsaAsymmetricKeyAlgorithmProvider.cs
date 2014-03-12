6094bee0e0b20588e6a778c13f6329ae8530a3cd
Andrew Arnott <andrewarnott@gmail.com>
2014-03-12 11:20:26 -0700

    RSA key generation now uses native iOS APIs.

---
+++ src/PCLCrypto.iOS/RsaAsymmetricKeyAlgorithmProvider.cs
@@ -0,0 +1,125 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RsaAsymmetricKeyAlgorithmProvider.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PCLCrypto
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MonoTouch;
+    using MonoTouch.Foundation;
+    using MonoTouch.ObjCRuntime;
+    using MonoTouch.Security;
+    using Validation;
+    using Platform = System.Security.Cryptography;
+
+    /// <summary>
+    /// The .NET Framework implementation of RSA.
+    /// </summary>
+    internal class RsaAsymmetricKeyAlgorithmProvider : IAsymmetricKeyAlgorithmProvider
+    {
+        /// <summary>
+        /// The algorithm used by this instance.
+        /// </summary>
+        private readonly AsymmetricAlgorithm algorithm;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RsaAsymmetricKeyAlgorithmProvider"/> class.
+        /// </summary>
+        /// <param name="algorithm">The algorithm.</param>
+        public RsaAsymmetricKeyAlgorithmProvider(AsymmetricAlgorithm algorithm)
+        {
+            this.algorithm = algorithm;
+        }
+
+        /// <inheritdoc/>
+        public AsymmetricAlgorithm Algorithm
+        {
+            get { return this.algorithm; }
+        }
+
+        /// <inheritdoc/>
+        public ICryptographicKey CreateKeyPair(int keySize)
+        {
+            Requires.Range(keySize > 0, "keySize");
+
+            NSString kSecAttrKeyType;
+            NSString kSecAttrKeySizeInBits;
+            NSString kSecAttrKeyTypeRSA;
+
+            var handle = Dlfcn.dlopen(Constants.SecurityLibrary, 0);
+            Assumes.False(handle == IntPtr.Zero);
+
+            try
+            {
+                kSecAttrKeyType = Dlfcn.GetStringConstant(handle, "kSecAttrKeyType");
+                kSecAttrKeySizeInBits = Dlfcn.GetStringConstant(handle, "kSecAttrKeySizeInBits");
+                kSecAttrKeyTypeRSA = Dlfcn.GetStringConstant(handle, "kSecAttrKeyTypeRSA");
+            }
+            finally
+            {
+                Dlfcn.dlclose(handle);
+            }
+
+            NSObject[] keys = new NSObject[] 
+            { 
+                kSecAttrKeyType, 
+                kSecAttrKeySizeInBits 
+            };
+            NSObject[] values = new NSObject[] 
+            { 
+                kSecAttrKeyTypeRSA, 
+                new NSNumber(keySize) 
+            };
+
+            SecKey publicKey;
+            SecKey privateKey;
+
+            NSDictionary parameters = NSDictionary.FromObjectsAndKeys(values, keys);
+
+            SecStatusCode code = SecKey.GenerateKeyPair(parameters, out publicKey, out privateKey);
+            Verify.Operation(code == SecStatusCode.Success, "status was " + code);
+
+            return new RsaCryptographicKey(publicKey, privateKey, this.algorithm);
+        }
+
+        /// <inheritdoc/>
+        public ICryptographicKey ImportKeyPair(byte[] keyBlob, CryptographicPrivateKeyBlobType blobType = CryptographicPrivateKeyBlobType.Pkcs8RawPrivateKeyInfo)
+        {
+            Requires.NotNull(keyBlob, "keyBlob");
+
+            SecKey privateKey, publicKey;
+            switch (blobType)
+            {
+                case CryptographicPrivateKeyBlobType.Capi1PrivateKey:
+                    throw new NotImplementedException();
+                default:
+                    throw new NotSupportedException();
+            }
+
+            return new RsaCryptographicKey(publicKey, privateKey, this.algorithm);
+        }
+
+        /// <inheritdoc/>
+        public ICryptographicKey ImportPublicKey(byte[] keyBlob, CryptographicPublicKeyBlobType blobType = CryptographicPublicKeyBlobType.X509SubjectPublicKeyInfo)
+        {
+            Requires.NotNull(keyBlob, "keyBlob");
+
+            SecKey publicKey;
+            switch (blobType)
+            {
+                case CryptographicPublicKeyBlobType.Capi1PublicKey:
+                    throw new NotImplementedException();
+                default:
+                    throw new NotSupportedException();
+            }
+
+            return new RsaCryptographicKey(publicKey, this.algorithm);
+        }
+    }
+}
---
+++ src/PCLCrypto.iOS/RsaCryptographicKey.cs
@@ -0,0 +1,139 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RsaCryptographicKey.cs" company="Andrew Arnott">
+//     Copyright (c) Andrew Arnott. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PCLCrypto
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MonoTouch.Security;
+    using Validation;
+
+    /// <summary>
+    /// The iOS implementation of the <see cref="ICryptographicKey"/> interface
+    /// for RSA keys.
+    /// </summary>
+    internal class RsaCryptographicKey : CryptographicKey, ICryptographicKey
+    {
+        /// <summary>
+        /// The platform crypto key.
+        /// </summary>
+        private readonly SecKey publicKey;
+
+        private readonly SecKey privateKey;
+
+        /// <summary>
+        /// The algorithm to use when performing cryptography.
+        /// </summary>
+        private readonly AsymmetricAlgorithm algorithm;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RsaCryptographicKey" /> class.
+        /// </summary>
+        /// <param name="publicKey">The public key.</param>
+        /// <param name="algorithm">The algorithm.</param>
+        internal RsaCryptographicKey(SecKey publicKey, AsymmetricAlgorithm algorithm)
+        {
+            Requires.NotNull(publicKey, "publicKey");
+
+            this.publicKey = publicKey;
+            this.algorithm = algorithm;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RsaCryptographicKey" /> class.
+        /// </summary>
+        /// <param name="publicKey">The public key.</param>
+        /// <param name="privateKey">The private key.</param>
+        /// <param name="algorithm">The algorithm.</param>
+        internal RsaCryptographicKey(SecKey publicKey, SecKey privateKey, AsymmetricAlgorithm algorithm)
+        {
+            Requires.NotNull(publicKey, "publicKey");
+            Requires.NotNull(privateKey, "privateKey");
+
+            this.publicKey = publicKey;
+            this.privateKey = privateKey;
+            this.algorithm = algorithm;
+        }
+
+        /// <inheritdoc />
+        public int KeySize
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        /// <summary>
+        /// Gets the algorithm to use with this key.
+        /// </summary>
+        internal AsymmetricAlgorithm Algorithm
+        {
+            get { return this.algorithm; }
+        }
+
+        /// <inheritdoc />
+        public byte[] Export(CryptographicPrivateKeyBlobType blobType)
+        {
+            switch (blobType)
+            {
+                case CryptographicPrivateKeyBlobType.Capi1PrivateKey:
+                ////return this.key.ExportCspBlob(includePrivateParameters: true);
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        /// <inheritdoc />
+        public byte[] ExportPublicKey(CryptographicPublicKeyBlobType blobType)
+        {
+            switch (blobType)
+            {
+                case CryptographicPublicKeyBlobType.Capi1PublicKey:
+                ////return this.key.ExportCspBlob(includePrivateParameters: false);
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        /// <inheritdoc />
+        protected internal override byte[] Sign(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        protected internal override bool VerifySignature(byte[] data, byte[] signature)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        protected internal override byte[] SignHash(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        protected internal override bool VerifyHash(byte[] data, byte[] signature)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        protected internal override byte[] Encrypt(byte[] data, byte[] iv)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        protected internal override byte[] Decrypt(byte[] data, byte[] iv)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
