AArnott/PCLCrypto

commit b0618cfa298b6accfc91db1cbd90db793f479244

Consolidates HashAlgorithmProvider into one shared file.

src/PCLCrypto.Shared.NetFx/HashAlgorithmProvider.cs
@@ -11,12 +11,14 @@ namespace PCLCrypto
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+#if Android
     using Java.Security;
+#endif
     using Validation;
     using Platform = System.Security.Cryptography;
 
     /// <summary>
-    /// The Java implementation of <see cref="IHashAlgorithmProvider"/>.
+    /// The .NET Framework implementation of <see cref="IHashAlgorithmProvider"/>.
     /// </summary>
     internal class HashAlgorithmProvider : IHashAlgorithmProvider
     {
@@ -45,9 +47,13 @@ namespace PCLCrypto
         {
             get
             {
-                using (var hash = CreateHashAlgorithm(this.Algorithm))
+                using (var hasher = CreateHashAlgorithm(this.Algorithm))
                 {
-                    return hash.DigestLength;
+#if Android
+                    return hasher.DigestLength / 8;
+#else
+                    return hasher.HashSize / 8;
+#endif
                 }
             }
         }
@@ -55,7 +61,11 @@ namespace PCLCrypto
         /// <inheritdoc />
         public CryptographicHash CreateHash()
         {
+#if Android
             return new JavaCryptographicHash(CreateHashAlgorithm(this.Algorithm));
+#else
+            return new NetFxCryptographicHash(CreateHashAlgorithm(this.Algorithm));
+#endif
         }
 
         /// <inheritdoc />
@@ -63,13 +73,22 @@ namespace PCLCrypto
         {
             Requires.NotNull(data, "data");
 
+#if Android
             using (var hasher = this.CreateHash())
             {
                 hasher.Append(data);
                 return hasher.GetValueAndReset();
             }
+#else
+            using (var hasher = CreateHashAlgorithm(this.Algorithm))
+            {
+                return hasher.ComputeHash(data);
+            }
+#endif
         }
 
+#if Android
+        
         /// <summary>
         /// Creates the hash algorithm.
         /// </summary>
@@ -95,5 +114,34 @@ namespace PCLCrypto
                     throw new NotSupportedException();
             }
         }
+
+#else
+
+        /// <summary>
+        /// Creates the hash algorithm.
+        /// </summary>
+        /// <param name="algorithm">The algorithm.</param>
+        /// <returns>
+        /// A platform-specific hash algorithm.
+        /// </returns>
+        internal static Platform.HashAlgorithm CreateHashAlgorithm(HashAlgorithm algorithm)
+        {
+#if SILVERLIGHT
+            switch (algorithm)
+            {
+                case HashAlgorithm.Sha1:
+                    return new Platform.SHA1Managed();
+                case HashAlgorithm.Sha256:
+                    return new Platform.SHA256Managed();
+                default:
+                    throw new NotSupportedException();
+           }
+#else
+            string algorithmName = HashAlgorithmProviderFactory.GetHashAlgorithmName(algorithm);
+            return Platform.HashAlgorithm.Create(algorithmName);
+#endif
+        }
+
+#endif
     }
 }

src/PCLCrypto.Desktop/HashAlgorithmProvider.cs
@@ -1,93 +1,0 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="HashAlgorithmProvider.cs" company="Andrew Arnott">
-//     Copyright (c) Andrew Arnott. All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------
-
-namespace PCLCrypto
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using Platform = System.Security.Cryptography;
-
-    /// <summary>
-    /// The .NET Framework implementation of <see cref="IHashAlgorithmProvider"/>.
-    /// </summary>
-    internal class HashAlgorithmProvider : IHashAlgorithmProvider
-    {
-        /// <summary>
-        /// The algorithm used by this instance.
-        /// </summary>
-        private readonly HashAlgorithm algorithm;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="HashAlgorithmProvider"/> class.
-        /// </summary>
-        /// <param name="algorithm">The algorithm.</param>
-        internal HashAlgorithmProvider(HashAlgorithm algorithm)
-        {
-            this.algorithm = algorithm;
-        }
-
-        /// <inheritdoc />
-        public HashAlgorithm Algorithm
-        {
-            get { return this.algorithm; }
-        }
-
-        /// <inheritdoc />
-        public int HashLength
-        {
-            get
-            {
-                using (var hasher = CreateHashAlgorithm(this.Algorithm))
-                {
-                    return hasher.HashSize / 8;
-                }
-            }
-        }
-
-        /// <inheritdoc />
-        public CryptographicHash CreateHash()
-        {
-            return new NetFxCryptographicHash(CreateHashAlgorithm(this.Algorithm));
-        }
-
-        /// <inheritdoc />
-        public byte[] HashData(byte[] data)
-        {
-            using (var hasher = CreateHashAlgorithm(this.Algorithm))
-            {
-                return hasher.ComputeHash(data);
-            }
-        }
-
-        /// <summary>
-        /// Creates the hash algorithm.
-        /// </summary>
-        /// <param name="algorithm">The algorithm.</param>
-        /// <returns>
-        /// A platform-specific hash algorithm.
-        /// </returns>
-        internal static Platform.HashAlgorithm CreateHashAlgorithm(HashAlgorithm algorithm)
-        {
-#if SILVERLIGHT
-            switch (algorithm)
-            {
-                case HashAlgorithm.Sha1:
-                    return new Platform.SHA1Managed();
-                case HashAlgorithm.Sha256:
-                    return new Platform.SHA256Managed();
-                default:
-                    throw new NotSupportedException();
-           }
-#else
-            string algorithmName = HashAlgorithmProviderFactory.GetHashAlgorithmName(algorithm);
-            return Platform.HashAlgorithm.Create(algorithmName);
-#endif
-        }
-    }
-}

src/PCLCrypto.Android/PCLCrypto.Android.csproj
@@ -36,7 +36,6 @@
     </Reference>
   </ItemGroup>
   <ItemGroup>
-    <Compile Include="HashAlgorithmProvider.cs" />
     <Compile Include="JavaCryptographicHashMac.cs" />
     <Compile Include="JavaCryptographicHash.cs" />
     <Compile Include="MacAlgorithmProvider.cs" />

src/PCLCrypto.Desktop/PCLCrypto.Desktop.csproj
@@ -39,7 +39,6 @@
   </ItemGroup>
   <ItemGroup>
     <Compile Include="CngAsymmetricKeyAlgorithmProvider.cs" />
-    <Compile Include="HashAlgorithmProvider.cs" />
     <Compile Include="NetFxCryptographicHash.cs" />
     <Compile Include="MacAlgorithmProvider.cs" />
     <Compile Include="RsaAsymmetricKeyAlgorithmProvider.cs" />

src/PCLCrypto.Shared.NetFx/PCLCrypto.Shared.NetFx.projitems
@@ -14,6 +14,7 @@
     <Compile Include="$(MSBuildThisFileDirectory)CryptographicEngine.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)CryptographicKey.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)DeriveBytes.cs" />
+    <Compile Include="$(MSBuildThisFileDirectory)HashAlgorithmProvider.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)KeyDerivationAlgorithmProvider.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)KeyDerivationAlgorithmProviderFactory.cs" />
     <Compile Include="$(MSBuildThisFileDirectory)KeyDerivationCryptographicKey.cs" />

src/PCLCrypto.Silverlight/PCLCrypto.Silverlight.csproj
@@ -69,9 +69,6 @@
     </ProjectReference>
   </ItemGroup>
   <ItemGroup>
-    <Compile Include="..\PCLCrypto.Desktop\HashAlgorithmProvider.cs">
-      <Link>HashAlgorithmProvider.cs</Link>
-    </Compile>
     <Compile Include="..\PCLCrypto.Desktop\MacAlgorithmProvider.cs">
       <Link>MacAlgorithmProvider.cs</Link>
     </Compile>

src/PCLCrypto.WindowsPhone/PCLCrypto.WindowsPhone.csproj
@@ -62,9 +62,6 @@
     </ProjectReference>
   </ItemGroup>
   <ItemGroup>
-    <Compile Include="..\PCLCrypto.Desktop\HashAlgorithmProvider.cs">
-      <Link>HashAlgorithmProvider.cs</Link>
-    </Compile>
     <Compile Include="..\PCLCrypto.Desktop\MacAlgorithmProvider.cs">
       <Link>MacAlgorithmProvider.cs</Link>
     </Compile>

src/PCLCrypto.iOS/PCLCrypto.iOS.csproj
@@ -54,9 +54,6 @@
     </ProjectReference>
   </ItemGroup>
   <ItemGroup>
-    <Compile Include="..\PCLCrypto.Desktop\HashAlgorithmProvider.cs">
-      <Link>HashAlgorithmProvider.cs</Link>
-    </Compile>
     <Compile Include="..\PCLCrypto.Desktop\MacAlgorithmProvider.cs">
       <Link>MacAlgorithmProvider.cs</Link>
     </Compile>

commit ac3ef240059313c5ea8fe016ba8794e3539ecf13

Fixes HashLength on Android.
This regression was introduced in b0618cfa29.

src/PCLCrypto.Shared.NetFx/HashAlgorithmProvider.cs
@@ -50,7 +50,7 @@ namespace PCLCrypto
                 using (var hasher = CreateHashAlgorithm(this.Algorithm))
                 {
 #if Android
-                    return hasher.DigestLength / 8;
+                    return hasher.DigestLength;
 #else
                     return hasher.HashSize / 8;
 #endif