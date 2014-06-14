379153453bb1c043a9f9a14c79ba5b747bfee04c
Andrew Arnott <andrewarnott@gmail.com>
2014-06-14 07:38:00 -0700

    Update to PCLTesting 0.1.2-Beta1.

--- src/PCLCrypto.Tests.Android/Resources/Resource.Designer.cs
+++ src/PCLCrypto.Tests.Android/Resources/Resource.Designer.cs
@@ -59,58 +59,14 @@ namespace PCLCrypto.Tests.Android
 			}
 		}
 		
-		public partial class Id
-		{
-			
-			// aapt resource value: 0x7f050004
-			public const int ResultsTextView = 2131034116;
-			
-			// aapt resource value: 0x7f050000
-			public const int RunTests = 2131034112;
-			
-			// aapt resource value: 0x7f050001
-			public const int progressBar1 = 2131034113;
-			
-			// aapt resource value: 0x7f050003
-			public const int scrollView1 = 2131034115;
-			
-			// aapt resource value: 0x7f050002
-			public const int summaryTextView = 2131034114;
-			
-			static Id()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Id()
-			{
-			}
-		}
-		
-		public partial class Layout
-		{
-			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
-			
-			static Layout()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Layout()
-			{
-			}
-		}
-		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f030001
+			public const int ApplicationName = 2130903041;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f030000
+			public const int Hello = 2130903040;
 			
 			static String()
 			{
