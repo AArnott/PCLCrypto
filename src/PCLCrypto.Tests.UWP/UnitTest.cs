3227f85c3d29fa487018b169d65fc3005c808356
Andrew Arnott <andarno@microsoft.com>
2015-10-15 17:25:45 -0700

    Add PCLCrypto.Tests.UWP test project

---
+++ src/PCLCrypto.Tests.UWP/UnitTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace PCLCrypto.Tests.UWP
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
