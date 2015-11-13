78a26584290f2e309762eea7b149ad9c8378cda2
Andrew Arnott <andarno@microsoft.com>
2015-11-13 08:22:26 -0800

    Throw derived exception type from reference assembly
    This should help better educate users and lead to more descriptive error reports when folks are using the reference assembly at runtime instead of the platform assembly.

---
+++ src/PCLCrypto/NotImplementedByReferenceAssemblyException.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Microsoft Public License (Ms-PL) license. See LICENSE file in the project root for full license information.
+
+namespace PCLCrypto
+{
+    using System;
+
+    /// <summary>
+    /// An exception thrown from the PCLCrypto reference assembly when it is called
+    /// instead of a platform-specific assembly at runtime.
+    /// </summary>
+    internal class NotImplementedByReferenceAssemblyException : NotImplementedException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NotImplementedByReferenceAssemblyException"/> class.
+        /// </summary>
+        internal NotImplementedByReferenceAssemblyException()
+            : base("This is a reference assembly and does not contain implementation. Be sure to install the PCLCrypto package into your application so the platform implementation assembly will be used at runtime.")
+        {
+        }
+    }
+}
