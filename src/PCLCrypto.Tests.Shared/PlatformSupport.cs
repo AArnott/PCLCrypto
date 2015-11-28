0a1c4b885228fc3c9b46e7bdccc85a1b00449e5e
Andrew Arnott <andarno@microsoft.com>
2015-11-27 20:15:57 -0800

    Add symmetric encryption support tests
    Not quite right, since it can't predict allowed key sizes

---
+++ src/PCLCrypto.Tests.Shared/PlatformSupport.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the Microsoft Public License (Ms-PL) license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PCLCrypto;
+using Xunit;
+
+public class PlatformSupport
+{
+    [SkippableTheory(typeof(NotSupportedException))]
+    [CombinatorialData]
+    public void SymmetricEncryption(SymmetricAlgorithmName name, SymmetricAlgorithmMode mode, SymmetricAlgorithmPadding padding)
+    {
+        using (var algorithm = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(name, mode, padding))
+        {
+            int keyLength = algorithm.BlockLength;
+            var keyMaterial = WinRTCrypto.CryptographicBuffer.GenerateRandom(keyLength);
+            using (var key = algorithm.CreateSymmetricKey(keyMaterial))
+            {
+                var ciphertext = WinRTCrypto.CryptographicEngine.Encrypt(key, new byte[algorithm.BlockLength], null);
+                Assert.Equal(algorithm.BlockLength, ciphertext.Length);
+            }
+        }
+    }
+}
