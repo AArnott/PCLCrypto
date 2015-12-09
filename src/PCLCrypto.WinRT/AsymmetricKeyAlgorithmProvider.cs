052fe1fc940688772f370362e83aec4ac4a5a1c0
Andrew Arnott <andarno@microsoft.com>
2015-12-08 22:45:43 -0800

    Work toward PCLCrypto.WinRT asymmetric keys use BCrypt

--- src/PCLCrypto.WinRT/AsymmetricKeyAlgorithmProvider.cs
+++ src/PCLCrypto.WinRT/AsymmetricKeyAlgorithmProvider.cs
@@ -9,6 +9,7 @@ namespace PCLCrypto
     using System.Text;
     using System.Threading.Tasks;
     using Validation;
+    using static PInvoke.BCrypt;
     using Platform = Windows.Security.Cryptography.Core;
 
     /// <summary>
@@ -17,30 +18,19 @@ namespace PCLCrypto
     internal class AsymmetricKeyAlgorithmProvider : IAsymmetricKeyAlgorithmProvider
     {
         /// <summary>
-        /// The WinRT platform implementation.
-        /// </summary>
-        private readonly Platform.AsymmetricKeyAlgorithmProvider platform;
-
-        /// <summary>
-        /// The algorithm used by this instance.
-        /// </summary>
-        private readonly AsymmetricAlgorithm algorithm;
-
-        /// <summary>
         /// Initializes a new instance of the <see cref="AsymmetricKeyAlgorithmProvider"/> class.
         /// </summary>
         /// <param name="algorithm">The algorithm.</param>
         public AsymmetricKeyAlgorithmProvider(AsymmetricAlgorithm algorithm)
         {
-            this.algorithm = algorithm;
-            this.platform = Platform.AsymmetricKeyAlgorithmProvider.OpenAlgorithm(GetAlgorithmName(algorithm));
+            this.Algorithm = algorithm;
+            this.AlgorithmHandle = BCryptOpenAlgorithmProvider(GetAlgorithmName(algorithm));
         }
 
-        /// <inheritdoc/>
-        public AsymmetricAlgorithm Algorithm
-        {
-            get { return this.algorithm; }
-        }
+        /// <summary>
+        /// Gets the algorithm.
+        /// </summary>
+        public AsymmetricAlgorithm Algorithm { get; }
 
         /// <inheritdoc/>
         public IReadOnlyList<KeySizes> LegalKeySizes
@@ -51,6 +41,11 @@ namespace PCLCrypto
                 return this.algorithm.GetTypicalLegalAsymmetricKeySizes();
             }
         }
+
+        /// <summary>
+        /// Gets the BCrypt algorithm.
+        /// </summary>
+        internal SafeAlgorithmHandle AlgorithmHandle { get; }
 
         /// <inheritdoc/>
         public ICryptographicKey CreateKeyPair(int keySize)
@@ -66,7 +61,7 @@ namespace PCLCrypto
         {
             Requires.NotNull(keyBlob, "keyBlob");
 
-            var key = this.platform.ImportKeyPair(keyBlob.ToBuffer(), GetPlatformKeyBlobType(blobType));
+            var key = BCryptImportKeyPair(this.AlgorithmHandle, AsymmetricKeyBlobTypes.EccPrivate, keyBlob, BCryptImportKeyPairFlags.None);
             return new WinRTCryptographicKey(key, canExportPrivateKey: true);
         }
 
@@ -75,7 +70,7 @@ namespace PCLCrypto
         {
             Requires.NotNull(keyBlob, "keyBlob");
 
-            var key = this.platform.ImportPublicKey(keyBlob.ToBuffer(), GetPlatformKeyBlobType(blobType));
+            var key = BCryptImportKeyPair(this.AlgorithmHandle, GetPlatformKeyBlobType(blobType), keyBlob);
             return new WinRTCryptographicKey(key, canExportPrivateKey: false);
         }
 
@@ -94,6 +89,7 @@ namespace PCLCrypto
         {
             if (disposing)
             {
+                this.AlgorithmHandle?.Dispose();
             }
         }
 
@@ -102,7 +98,7 @@ namespace PCLCrypto
         /// </summary>
         /// <param name="blobType">The platform independent enum value for the blob type.</param>
         /// <returns>The platform-specific enum value for the equivalent blob type.</returns>
-        internal static Platform.CryptographicPublicKeyBlobType GetPlatformKeyBlobType(CryptographicPublicKeyBlobType blobType)
+        internal static string GetPlatformKeyBlobType(CryptographicPublicKeyBlobType blobType)
         {
             switch (blobType)
             {
@@ -151,41 +147,28 @@ namespace PCLCrypto
             switch (algorithm)
             {
                 case AsymmetricAlgorithm.DsaSha1:
-                    return Platform.AsymmetricAlgorithmNames.DsaSha1;
                 case AsymmetricAlgorithm.DsaSha256:
-                    return Platform.AsymmetricAlgorithmNames.DsaSha256;
+                    return AlgorithmIdentifiers.BCRYPT_DSA_ALGORITHM;
                 case AsymmetricAlgorithm.EcdsaP256Sha256:
-                    return Platform.AsymmetricAlgorithmNames.EcdsaP256Sha256;
+                    return AlgorithmIdentifiers.BCRYPT_ECDSA_P256_ALGORITHM;
                 case AsymmetricAlgorithm.EcdsaP384Sha384:
-                    return Platform.AsymmetricAlgorithmNames.EcdsaP384Sha384;
+                    return AlgorithmIdentifiers.BCRYPT_ECDSA_P384_ALGORITHM;
                 case AsymmetricAlgorithm.EcdsaP521Sha512:
-                    return Platform.AsymmetricAlgorithmNames.EcdsaP521Sha512;
+                    return AlgorithmIdentifiers.BCRYPT_ECDSA_P521_ALGORITHM;
                 case AsymmetricAlgorithm.RsaOaepSha1:
-                    return Platform.AsymmetricAlgorithmNames.RsaOaepSha1;
                 case AsymmetricAlgorithm.RsaOaepSha256:
-                    return Platform.AsymmetricAlgorithmNames.RsaOaepSha256;
                 case AsymmetricAlgorithm.RsaOaepSha384:
-                    return Platform.AsymmetricAlgorithmNames.RsaOaepSha384;
                 case AsymmetricAlgorithm.RsaOaepSha512:
-                    return Platform.AsymmetricAlgorithmNames.RsaOaepSha512;
                 case AsymmetricAlgorithm.RsaPkcs1:
-                    return Platform.AsymmetricAlgorithmNames.RsaPkcs1;
                 case AsymmetricAlgorithm.RsaSignPkcs1Sha1:
-                    return Platform.AsymmetricAlgorithmNames.RsaSignPkcs1Sha1;
                 case AsymmetricAlgorithm.RsaSignPkcs1Sha256:
-                    return Platform.AsymmetricAlgorithmNames.RsaSignPkcs1Sha256;
                 case AsymmetricAlgorithm.RsaSignPkcs1Sha384:
-                    return Platform.AsymmetricAlgorithmNames.RsaSignPkcs1Sha384;
                 case AsymmetricAlgorithm.RsaSignPkcs1Sha512:
-                    return Platform.AsymmetricAlgorithmNames.RsaSignPkcs1Sha512;
                 case AsymmetricAlgorithm.RsaSignPssSha1:
-                    return Platform.AsymmetricAlgorithmNames.RsaSignPssSha1;
                 case AsymmetricAlgorithm.RsaSignPssSha256:
-                    return Platform.AsymmetricAlgorithmNames.RsaSignPssSha256;
                 case AsymmetricAlgorithm.RsaSignPssSha384:
-                    return Platform.AsymmetricAlgorithmNames.RsaSignPssSha384;
                 case AsymmetricAlgorithm.RsaSignPssSha512:
-                    return Platform.AsymmetricAlgorithmNames.RsaSignPssSha512;
+                    return AlgorithmIdentifiers.BCRYPT_RSA_ALGORITHM;
                 default:
                     throw new NotSupportedException();
             }
