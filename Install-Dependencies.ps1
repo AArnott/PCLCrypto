21a434ebd9bbc6e7fe93417c2d687236cf30efbc
Andrew Arnott <andrewarnott@gmail.com>
2016-02-06 08:36:41 -0800

    Clean up output of installing dependencies

--- Install-Dependencies.ps1
+++ Install-Dependencies.ps1
@@ -27,7 +27,11 @@ Function Install-AndroidSDK() {
 
     $sdkIndexes = $sdks |% { $_.Index }
     $sdkIndexArgument = [string]::Join(',',  $sdkIndexes)
-    Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument
+    Write-Output "Installing additional Android SDKs..."
+    $sdks | Format-Table Name
+
+    # Suppress the output to STDOUT
+    $null = Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument
 }
 
 $sdks = Get-AndroidSDKs |? { $_.name -like 'sdk platform*API 10*' -or $_.name -like 'google apis*api 10' }
