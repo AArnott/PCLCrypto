AArnott/PCLCrypto

commit de2dfa4eea997381c58299f9f46ec0650d764a95

Remove IDisposable from ISymmetricKeyAlgorithmProvider

src/PCLCrypto.WinRT/SymmetricKeyAlgorithmProvider.cs
@@ -34,22 +34,18 @@ namespace PCLCrypto
             this.Name = name;
             this.Mode = mode;
             this.Padding = padding;
-
-            this.Algorithm = BCryptOpenAlgorithmProvider(GetAlgorithmName(name));
-            try
-            {
-                BCryptSetProperty(this.Algorithm, PropertyNames.BCRYPT_CHAINING_MODE, GetChainingMode(mode));
-            }
-            catch (PInvoke.NTStatusException ex)
-            {
-                throw new ArgumentException(ex.Message, ex);
-            }
         }
 
         /// <inheritdoc/>
         public int BlockLength
         {
-            get { return BCryptGetProperty<int>(this.Algorithm, PropertyNames.BCRYPT_BLOCK_LENGTH); }
+            get
+            {
+                using (var algorithm = this.OpenAlgorithm())
+                {
+                    return BCryptGetProperty<int>(algorithm, PropertyNames.BCRYPT_BLOCK_LENGTH);
+                }
+            }
         }
 
         /// <inheritdoc/>
@@ -59,22 +55,20 @@ namespace PCLCrypto
             {
                 if (this.legalKeySizes == null)
                 {
-                    var keyLengths = BCryptGetProperty<BCRYPT_KEY_LENGTHS_STRUCT>(this.Algorithm, PropertyNames.BCRYPT_KEY_LENGTHS);
-                    this.legalKeySizes = new ReadOnlyCollection<KeySizes>(
-                        new[]
-                        {
-                            new KeySizes(keyLengths.MinLength, keyLengths.MaxLength, keyLengths.Increment),
-                        });
+                    using (var algorithm = this.OpenAlgorithm())
+                    {
+                        var keyLengths = BCryptGetProperty<BCRYPT_KEY_LENGTHS_STRUCT>(algorithm, PropertyNames.BCRYPT_KEY_LENGTHS);
+                        this.legalKeySizes = new ReadOnlyCollection<KeySizes>(
+                            new[]
+                            {
+                                new KeySizes(keyLengths.MinLength, keyLengths.MaxLength, keyLengths.Increment),
+                            });
+                    }
                 }
 
                 return this.legalKeySizes;
             }
         }
-
-        /// <summary>
-        /// Gets the BCrypt algorithm.
-        /// </summary>
-        internal SafeAlgorithmHandle Algorithm { get; }
 
         /// <inheritdoc/>
         public ICryptographicKey CreateSymmetricKey(byte[] keyMaterial)
@@ -85,11 +79,23 @@ namespace PCLCrypto
         }
 
         /// <summary>
-        /// Disposes resources associated with this instance.
+        /// Initializes a new <see cref="SafeAlgorithmHandle"/> to represent
+        /// this algorithm.
         /// </summary>
-        public void Dispose()
+        /// <returns>The new <see cref="SafeAlgorithmHandle"/>.</returns>
+        protected internal SafeAlgorithmHandle OpenAlgorithm()
         {
-            this.Algorithm.Dispose();
+            var algorithm = BCryptOpenAlgorithmProvider(GetAlgorithmName(this.Name));
+            try
+            {
+                BCryptSetProperty(algorithm, PropertyNames.BCRYPT_CHAINING_MODE, GetChainingMode(this.Mode));
+                return algorithm;
+            }
+            catch (PInvoke.NTStatusException ex)
+            {
+                algorithm.Dispose();
+                throw new ArgumentException(ex.Message, ex);
+            }
         }
 
         /// <summary>

src/PCLCrypto.Android/SymmetricKeyAlgorithmProvider.cs
@@ -113,13 +113,6 @@ namespace PCLCrypto
         }
 
         /// <summary>
-        /// Disposes resources associated with this instance.
-        /// </summary>
-        public void Dispose()
-        {
-        }
-
-        /// <summary>
         /// Gets the block size (in bytes) for the specified algorithm.
         /// </summary>
         /// <param name="mode">The algorithm mode.</param>

src/PCLCrypto.Shared.NetFxSymmetric/SymmetricKeyAlgorithmProvider.cs
@@ -91,13 +91,6 @@ namespace PCLCrypto
             return new SymmetricCryptographicKey(platform, this.Name, this.Mode, this.Padding);
         }
 
-        /// <summary>
-        /// Disposes resources associated with this instance.
-        /// </summary>
-        public void Dispose()
-        {
-        }
-
 #if !SILVERLIGHT
         /// <summary>
         /// Gets the platform enum value for the block mode used by the specified algorithm.

src/PCLCrypto.Shared.Common/ISymmetricKeyAlgorithmProvider.cs
@@ -11,7 +11,7 @@ namespace PCLCrypto
     /// <summary>
     /// Represents a provider of symmetric key algorithms.
     /// </summary>
-    public interface ISymmetricKeyAlgorithmProvider : IDisposable
+    public interface ISymmetricKeyAlgorithmProvider
     {
         /// <summary>
         /// Gets the size, in bytes, of the cipher block for the open algorithm.

src/PCLCrypto.Tests.Shared/CryptographicEngineTests.cs
@@ -110,13 +110,11 @@ public class CryptographicEngineTests
         // the EncryptAndAuthenticate method.
         try
         {
-            using (var algorithm = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithm.AesCcm))
+            var algorithm = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithm.AesCcm);
+            using (var aesCcmKey = algorithm.CreateSymmetricKey(Convert.FromBase64String(AesKeyMaterial)))
             {
-                using (var aesCcmKey = algorithm.CreateSymmetricKey(Convert.FromBase64String(AesKeyMaterial)))
-                {
-                    Assert.Throws<InvalidOperationException>(() => WinRTCrypto.CryptographicEngine.Encrypt(aesCcmKey, new byte[16]));
-                    Assert.Throws<InvalidOperationException>(() => WinRTCrypto.CryptographicEngine.Decrypt(aesCcmKey, new byte[16]));
-                }
+                Assert.Throws<InvalidOperationException>(() => WinRTCrypto.CryptographicEngine.Encrypt(aesCcmKey, new byte[16]));
+                Assert.Throws<InvalidOperationException>(() => WinRTCrypto.CryptographicEngine.Decrypt(aesCcmKey, new byte[16]));
             }
         }
         catch (NotSupportedException)
@@ -126,13 +124,11 @@ public class CryptographicEngineTests
 
         try
         {
-            using (var algorithm = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithm.AesGcm))
+            var algorithm = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithm.AesGcm);
+            using (var aesGcmKey = algorithm.CreateSymmetricKey(Convert.FromBase64String(AesKeyMaterial)))
             {
-                using (var aesGcmKey = algorithm.CreateSymmetricKey(Convert.FromBase64String(AesKeyMaterial)))
-                {
-                    Assert.Throws<InvalidOperationException>(() => WinRTCrypto.CryptographicEngine.Encrypt(aesGcmKey, new byte[16]));
-                    Assert.Throws<InvalidOperationException>(() => WinRTCrypto.CryptographicEngine.Decrypt(aesGcmKey, new byte[16]));
-                }
+                Assert.Throws<InvalidOperationException>(() => WinRTCrypto.CryptographicEngine.Encrypt(aesGcmKey, new byte[16]));
+                Assert.Throws<InvalidOperationException>(() => WinRTCrypto.CryptographicEngine.Decrypt(aesGcmKey, new byte[16]));
             }
         }
         catch (NotSupportedException)
@@ -192,43 +188,41 @@ public class CryptographicEngineTests
         byte[] iv = IV;
         byte[] plaintext = new byte[inputLength];
         Array.Copy(this.bigData, plaintext, inputLength);
-        using (var algorithm = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmName.Aes, SymmetricAlgorithmMode.Cbc, padding))
+        var algorithm = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmName.Aes, SymmetricAlgorithmMode.Cbc, padding);
+        using (var key = algorithm.CreateSymmetricKey(Convert.FromBase64String(AesKeyMaterial)))
         {
-            using (var key = algorithm.CreateSymmetricKey(Convert.FromBase64String(AesKeyMaterial)))
+            if (expectedCiphertext == null)
             {
-                if (expectedCiphertext == null)
-                {
-                    Assert.Throws<ArgumentException>(
-                        () => WinRTCrypto.CryptographicEngine.Encrypt(key, plaintext, iv));
-                }
-                else
-                {
-                    byte[] actualCipherText = WinRTCrypto.CryptographicEngine.Encrypt(key, plaintext, iv);
-                    Assert.Equal(
-                        expectedCiphertext,
-                        Convert.ToBase64String(actualCipherText));
+                Assert.Throws<ArgumentException>(
+                    () => WinRTCrypto.CryptographicEngine.Encrypt(key, plaintext, iv));
+            }
+            else
+            {
+                byte[] actualCipherText = WinRTCrypto.CryptographicEngine.Encrypt(key, plaintext, iv);
+                Assert.Equal(
+                    expectedCiphertext,
+                    Convert.ToBase64String(actualCipherText));
 
-                    byte[] expectedPlainText = plaintext;
-                    if (!PaddingPreservesPlaintextLength(padding))
+                byte[] expectedPlainText = plaintext;
+                if (!PaddingPreservesPlaintextLength(padding))
+                {
+                    // Therefore the expected decrypted value will have a length that is a multiple
+                    // of the block length.
+                    int blockLength = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmName.Aes, SymmetricAlgorithmMode.Cbc, SymmetricAlgorithmPadding.Zeros)
+                        .BlockLength;
+                    int bytesBeyondLastBlockLength = expectedPlainText.Length % blockLength;
+                    if (bytesBeyondLastBlockLength > 0)
                     {
-                        // Therefore the expected decrypted value will have a length that is a multiple
-                        // of the block length.
-                        int blockLength = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmName.Aes, SymmetricAlgorithmMode.Cbc, SymmetricAlgorithmPadding.Zeros)
-                            .BlockLength;
-                        int bytesBeyondLastBlockLength = expectedPlainText.Length % blockLength;
-                        if (bytesBeyondLastBlockLength > 0)
-                        {
-                            int growBy = blockLength - bytesBeyondLastBlockLength;
-                            Array.Resize(ref expectedPlainText, expectedPlainText.Length + growBy);
-                        }
+                        int growBy = blockLength - bytesBeyondLastBlockLength;
+                        Array.Resize(ref expectedPlainText, expectedPlainText.Length + growBy);
                     }
-
-                    byte[] actualPlainText = WinRTCrypto.CryptographicEngine.Decrypt(key, actualCipherText, iv);
-
-                    Assert.Equal(
-                        Convert.ToBase64String(expectedPlainText),
-                        Convert.ToBase64String(actualPlainText));
                 }
+
+                byte[] actualPlainText = WinRTCrypto.CryptographicEngine.Decrypt(key, actualCipherText, iv);
+
+                Assert.Equal(
+                    Convert.ToBase64String(expectedPlainText),
+                    Convert.ToBase64String(actualPlainText));
             }
         }
     }

src/PCLCrypto.Tests.Shared/PlatformSupport.cs
@@ -38,15 +38,13 @@ public class PlatformSupport
             return;
         }
 
-        using (var algorithm = creator())
+        var algorithm = creator();
+        int keyLength = algorithm.LegalKeySizes.First().MinSize;
+        var keyMaterial = WinRTCrypto.CryptographicBuffer.GenerateRandom(keyLength / 8);
+        using (var key = algorithm.CreateSymmetricKey(keyMaterial))
         {
-            int keyLength = algorithm.LegalKeySizes.First().MinSize;
-            var keyMaterial = WinRTCrypto.CryptographicBuffer.GenerateRandom(keyLength / 8);
-            using (var key = algorithm.CreateSymmetricKey(keyMaterial))
-            {
-                var ciphertext = WinRTCrypto.CryptographicEngine.Encrypt(key, new byte[algorithm.BlockLength], null);
-                Assert.NotEqual(0, ciphertext.Length);
-            }
+            var ciphertext = WinRTCrypto.CryptographicEngine.Encrypt(key, new byte[algorithm.BlockLength], null);
+            Assert.NotEqual(0, ciphertext.Length);
         }
     }
 

src/PCLCrypto.Tests.Shared/SymmetricKeyAlgorithmProviderTests.cs
@@ -43,52 +43,50 @@ public class SymmetricKeyAlgorithmProviderTests
     {
         var blockMode = name.IsBlockCipher() ? SymmetricAlgorithmMode.Cbc : SymmetricAlgorithmMode.Streaming;
         var padding = name.IsBlockCipher() ? SymmetricAlgorithmPadding.PKCS7 : SymmetricAlgorithmPadding.None;
-        using (ISymmetricKeyAlgorithmProvider provider = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(name, blockMode, padding))
+        ISymmetricKeyAlgorithmProvider provider = WinRTCrypto.SymmetricKeyAlgorithmProvider.OpenAlgorithm(name, blockMode, padding);
+        var result = provider.LegalKeySizes;
+        Assert.NotNull(result);
+        Assert.NotEmpty(result);
+
+        var random = new Random();
+        Action<int> attemptKeySize = size =>
         {
-            var result = provider.LegalKeySizes;
-            Assert.NotNull(result);
-            Assert.NotEmpty(result);
-
-            var random = new Random();
-            Action<int> attemptKeySize = size =>
-            {
-                var keyMaterial = new byte[size / 8];
-                random.NextBytes(keyMaterial); // some algorithms check against weak keys (e.g. all zeros)
+            var keyMaterial = new byte[size / 8];
+            random.NextBytes(keyMaterial); // some algorithms check against weak keys (e.g. all zeros)
                 provider.CreateSymmetricKey(keyMaterial).Dispose();
-            };
+        };
 
-            // Verify that each allegedly legal key size actually works.
-            foreach (var item in result)
+        // Verify that each allegedly legal key size actually works.
+        foreach (var item in result)
+        {
+            this.logger.WriteLine($"{item.MinSize}-{item.MaxSize} ({item.StepSize})");
+            foreach (var keySize in item)
             {
-                this.logger.WriteLine($"{item.MinSize}-{item.MaxSize} ({item.StepSize})");
-                foreach (var keySize in item)
-                {
-                    attemptKeySize(keySize);
-                }
-
-                // Also check the cases just off the edges of the range to see that they actually fail.
-                // This ensures the returned values aren't too conservative.
-#if false // WinRT actually doesn't throw when given keys of inappropriate size. Go figure.
-                if (item.StepSize > 0)
-                {
-                    if (item.MinSize - item.StepSize > 0)
-                    {
-                        Assert.Throws<ArgumentException>(() => attemptKeySize(item.MinSize - item.StepSize));
-                    }
-
-                    if (item.MaxSize + item.StepSize > 0)
-                    {
-                        Assert.Throws<ArgumentException>(() => attemptKeySize(item.MaxSize + item.StepSize));
-                    }
-                }
-#endif
+                attemptKeySize(keySize);
             }
 
-            var range = result.Single();
-            Assert.Equal(minSize, range.MinSize);
-            Assert.Equal(maxSize, range.MaxSize);
-            Assert.Equal(stepSize, range.StepSize);
+            // Also check the cases just off the edges of the range to see that they actually fail.
+            // This ensures the returned values aren't too conservative.
+#if false // WinRT actually doesn't throw when given keys of inappropriate size. Go figure.
+            if (item.StepSize > 0)
+            {
+                if (item.MinSize - item.StepSize > 0)
+                {
+                    Assert.Throws<ArgumentException>(() => attemptKeySize(item.MinSize - item.StepSize));
+                }
+
+                if (item.MaxSize + item.StepSize > 0)
+                {
+                    Assert.Throws<ArgumentException>(() => attemptKeySize(item.MaxSize + item.StepSize));
+                }
+            }
+#endif
         }
+
+        var range = result.Single();
+        Assert.Equal(minSize, range.MinSize);
+        Assert.Equal(maxSize, range.MaxSize);
+        Assert.Equal(stepSize, range.StepSize);
     }
 
     [Fact]

src/PCLCrypto.WinRT/SymmetricCryptographicKey.cs
@@ -276,7 +276,10 @@ namespace PCLCrypto
                 key?.Dispose();
                 try
                 {
-                    key = BCryptGenerateSymmetricKey(this.symmetricAlgorithmProvider.Algorithm, this.keyMaterial);
+                    using (var algorithm = this.symmetricAlgorithmProvider.OpenAlgorithm())
+                    {
+                        key = BCryptGenerateSymmetricKey(algorithm, this.keyMaterial);
+                    }
                 }
                 catch (NTStatusException ex)
                 {
@@ -301,10 +304,13 @@ namespace PCLCrypto
 
         private SafeKeyHandle CreateKey()
         {
-            return BCryptImportKey(
-                this.symmetricAlgorithmProvider.Algorithm,
-                SymmetricKeyBlobTypes.BCRYPT_KEY_DATA_BLOB,
-                this.keyMaterial);
+            using (var algorithm = this.symmetricAlgorithmProvider.OpenAlgorithm())
+            {
+                return BCryptImportKey(
+                    algorithm,
+                    SymmetricKeyBlobTypes.BCRYPT_KEY_DATA_BLOB,
+                    this.keyMaterial);
+            }
         }
 
         private abstract class BCryptCipherTransform : ICryptoTransform

commit db8cf4c735585216e08f5d7d3d0c4017dc69a712

Fix test failure from not throwing for invalid algorithm parameter combinations
This regressed with de2dfa4e

src/PCLCrypto.WinRT/SymmetricKeyAlgorithmProvider.cs
@@ -34,6 +34,11 @@ namespace PCLCrypto
             this.Name = name;
             this.Mode = mode;
             this.Padding = padding;
+
+            // Try opening the algorithm now to throw any exceptions that it may.
+            using (this.OpenAlgorithm())
+            {
+            }
         }
 
         /// <inheritdoc/>