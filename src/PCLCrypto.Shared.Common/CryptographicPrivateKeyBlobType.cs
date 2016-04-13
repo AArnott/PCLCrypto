73cd301233f73329868ee2ae3ca2e48578c3fc06
Andrew Arnott <andrewarnott@gmail.com>
2016-04-13 08:10:35 -0700

    Fix RFC reference in documentation
    Fixes #94

--- src/PCLCrypto.Shared.Common/CryptographicPrivateKeyBlobType.cs
+++ src/PCLCrypto.Shared.Common/CryptographicPrivateKeyBlobType.cs
@@ -19,7 +19,7 @@ namespace PCLCrypto
         Pkcs8RawPrivateKeyInfo,
 
         /// <summary>
-        /// The key is an RSA private key defined in the PKCS #1 standard. For more information, see the RSA Cryptography Specification in RFC 3347.
+        /// The key is an RSA private key defined in the PKCS #1 standard. For more information, see the RSA Cryptography Specification in RFC 3447.
         /// </summary>
         Pkcs1RsaPrivateKey,
 
