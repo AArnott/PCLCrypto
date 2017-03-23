f2ff68b0f661c90521ca9cf14bbdd304ac6a757a
Andrew Arnott <andrewarnott@gmail.com>
2017-03-22 22:51:00 -0700

    Remove unused tools and update appveyor

--- Install-Dependencies.ps1
+++ Install-Dependencies.ps1
@@ -34,6 +34,20 @@ Function Install-AndroidSDK() {
     $null = Echo 'y' | & $AndroidToolPath update sdk -u -a -t $sdkIndexArgument
 }
 
+Function Install-Win81SDK {
+    if (!(Test-Path "$PSScriptRoot\obj")) { $null = mkdir "$PSScriptRoot\obj" }
+    $sdkSetupPath = "$PSScriptRoot\obj\Win8SDKSetup.exe"
+    $sdkLogFile = "$PSScriptRoot\obj\win81sdk.log"
+    if (!(Test-Path $sdkSetupPath)) {
+        Write-Output "Downloading the Windows 8.1 SDK..."
+        Invoke-WebRequest -Uri "http://go.microsoft.com/fwlink/p/?LinkId=323507" -OutFile $sdkSetupPath
+    }
+
+    Write-Output "Installing the Windows 8.1 SDK..."
+    Start-Process $sdkSetupPath @('/features','+','/q','/l',$sdkLogFile)  -Wait
+}
+
 $sdks = Get-AndroidSDKs |? { $_.name -like 'sdk platform*API 10*' -or $_.name -like 'google apis*api 10' }
 
+Install-Win81SDK
 Install-AndroidSDK -sdks $sdks
