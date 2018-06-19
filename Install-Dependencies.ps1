82c47f903e9fc116022e5c83e9e7f2e52836b7e0
Andrew Arnott <andrewarnott@gmail.com>
2018-06-19 09:09:35 -0700

    Accept license

--- Install-Dependencies.ps1
+++ Install-Dependencies.ps1
@@ -1,7 +1,15 @@
+[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='high')]
+Param(
+)
+
 if (-not "$env:JAVA_HOME") {
-    Invoke-WebRequest -OutFile "$env:TEMP\jre.exe" http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jre-10.0.1_windows-x64_bin.exe
-    Start-Process "$env:TEMP\jre.exe" @('INSTALL_SILENT=Enable', 'INSTALL_DIR=C:\Program Files (x86)\Java') -Wait
-    $env:JAVA_HOME = "C:\Program Files (x86)\Java"
+    if ($PSCmdlet.ShouldProcess("Install JRE")) {
+        Write-Host "Downloading Java Runtime Environment" -ForegroundColor Yellow
+        Invoke-WebRequest -OutFile "$env:TEMP\jre.exe" http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jre-10.0.1_windows-x64_bin.exe
+        Write-Host "Installing Java Runtime Environment" -ForegroundColor Yellow
+        Start-Process "$env:TEMP\jre.exe" @('INSTALL_SILENT=Enable', 'INSTALL_DIR=C:\Program Files (x86)\Java') -Wait
+        $env:JAVA_HOME = "C:\Program Files (x86)\Java"
+    }
 }
 
 $AndroidToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\bin\sdkmanager.bat"
@@ -15,13 +23,19 @@ Function Install-Win81SDK {
     $sdkSetupPath = "$PSScriptRoot\obj\Win8SDKSetup.exe"
     $sdkLogFile = "$PSScriptRoot\obj\win81sdk.log"
     if (!(Test-Path $sdkSetupPath)) {
-        Write-Output "Downloading the Windows 8.1 SDK..."
+        Write-Host "Downloading the Windows 8.1 SDK..." -ForegroundColor Yellow
         Invoke-WebRequest -Uri "http://go.microsoft.com/fwlink/p/?LinkId=323507" -OutFile $sdkSetupPath
     }
 
-    Write-Output "Installing the Windows 8.1 SDK..."
+    Write-Host "Installing the Windows 8.1 SDK..." -ForegroundColor Yellow
     Start-Process $sdkSetupPath @('/features','+','/q','/l',$sdkLogFile)  -Wait
 }
 
-Install-Win81SDK
-& $AndroidToolPath "platforms;android-10"
+if ($PSCmdlet.ShouldProcess("Install Windows 8.1 SDK")) {
+    Install-Win81SDK
+}
+
+if ($PSCmdlet.ShouldProcess("Install Android SDK 10")) {
+    Write-Host "Installing Android SDK" -ForegroundColor Yellow
+    Write-Output "y" | & $AndroidToolPath "platforms;android-10"
+}
