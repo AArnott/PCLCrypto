b1965daea7d4e6cf19292b1cb0e27920da8a62e4
Andrew Arnott <andrewarnott@gmail.com>
2018-06-19 09:13:41 -0700

    Remove ShouldProcess calls

--- Install-Dependencies.ps1
+++ Install-Dependencies.ps1
@@ -3,13 +3,13 @@ Param(
 )
 
 if (-not "$env:JAVA_HOME") {
-    if ($PSCmdlet.ShouldProcess("Install JRE")) {
+ #   if ($PSCmdlet.ShouldProcess("Install JRE")) {
         Write-Host "Downloading Java Runtime Environment" -ForegroundColor Yellow
         Invoke-WebRequest -OutFile "$env:TEMP\jre.exe" http://download.oracle.com/otn-pub/java/jdk/10.0.1+10/fb4372174a714e6b8c52526dc134031e/jre-10.0.1_windows-x64_bin.exe
         Write-Host "Installing Java Runtime Environment" -ForegroundColor Yellow
         Start-Process "$env:TEMP\jre.exe" @('INSTALL_SILENT=Enable', 'INSTALL_DIR=C:\Program Files (x86)\Java') -Wait
         $env:JAVA_HOME = "C:\Program Files (x86)\Java"
-    }
+#    }
 }
 
 $AndroidToolPath = "${env:ProgramFiles(x86)}\Android\android-sdk\tools\bin\sdkmanager.bat"
@@ -31,11 +31,11 @@ Function Install-Win81SDK {
     Start-Process $sdkSetupPath @('/features','+','/q','/l',$sdkLogFile)  -Wait
 }
 
-if ($PSCmdlet.ShouldProcess("Install Windows 8.1 SDK")) {
+#if ($PSCmdlet.ShouldProcess("Install Windows 8.1 SDK")) {
     Install-Win81SDK
-}
+#}
 
-if ($PSCmdlet.ShouldProcess("Install Android SDK 10")) {
+#if ($PSCmdlet.ShouldProcess("Install Android SDK 10")) {
     Write-Host "Installing Android SDK" -ForegroundColor Yellow
     Write-Output "y" | & $AndroidToolPath "platforms;android-10"
-}
+#}
